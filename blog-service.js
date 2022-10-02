ec38e1165eb8ad1a40d65cef8397fb628c43379e
Smit Patel <sonu9408394224@gmail.com>
2022-10-02 02:53:13 -0400

    ini

--- blog-service.js
+++ blog-service.js
@@ -1,15 +1,30 @@
+//the fs.readFile method
+
 const fs = require('fs');
+
+//•	posts - type: array
+
 let posts = [];
+
+//•	categories - type: array
 let categories = [];
 
 
 
 module.exports.initialize = function () {
     return new Promise((resolve, reject) => {
+
+      /*fs.readFile('somefile.json', 'utf8', (err, data) => {
+     if (err) throw err;
+     console.log(data);
+    }); */
+  
       fs.readFile('./data/posts.json', (err, data) => {
         if (err) {
           reject(err);
         }
+        //assign that array to the posts array 
+
         posts = JSON.parse(data);
         resolve();
       });
@@ -23,29 +38,54 @@ module.exports.initialize = function () {
     });
   };
   
+
+  //•	This function will provide the full array of "posts" objects using the resolve method of the 
+  //returned promise.  
+
   module.exports.getAllposts = function () {
-    return new Promise((resolve, reject) => {
+
+    return new Promise(
+        (resolve, reject) => {
+
       if (posts.length == 0) {
-        reject('NO RESULTS RETURNED');
+
+        //the reject method 
+        reject('NO RESULTS RETURNED'
+        );
       }
       resolve(posts);
     });
   };
   
-  module.exports.getPublishedPosts() = function () {
-    return new Promise((resolve, reject) => {
-      var published = [];
-      for (let len = 0; len < posts.length; len++) {
-        if (posts[len].ispublished == true) {
-          published.push(posts[len]);
+  //getPublishedPosts()
+  /**•	This function will provide an array of "post" objects whose published property is true using the resolve method of the returned promise.  / */
+
+  module.exports.getPublishedPosts = function ()
+   {
+    return new Promise((resolve, reject) => 
+    {
+      var published = posts.filter
+      (f=>f.published==true);
+//this function must invoke the reject method and pass a meaningful message, i.e. "no results returned".
+      for (let len = 0; len < posts.length; len++) 
+      {
+        if (posts[len].ispublished == true)
+         {
+           published.push(posts[len]);
         }
       }
-      if (published.length == 0) {
-        reject('NO RESULTS RETURNED');
+
+       if (published.length === 0)
+        {
+         reject('NO RESULTS RETURNED');
       }
       resolve(posts);
     });
   };
+  
+//getCategories()
+//•	This function will provide the full array of "category" objects using the resolve method of the 
+//returned promise.  
   
   module.exports.getCategories = function () {
     return new Promise((resolve, reject) => {
--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-const service = require('./blog-service');
+
  const express = require('express');
+
  const path = require('path');
- 
+
+ const service = require('./blog-service');
  
  const app = express();
  
+ //"TODO: get all posts who have published==true"
  const HTTP_PORT = process.env.port || 8080;
+ //	The server must listen on process.env.PORT || 8080
+
+ function onHttpStart(){
+
+    console.log(
+        "Express http server listening on "+HTTP_PORT
+        
+    );
+}
  
  app.use(express.static('public'));
  
  app.get('/', (req, res) => {
+
      res.redirect('/about');
  })
  
@@ -17,29 +30,48 @@ const service = require('./blog-service');
      res.sendFile(path.join(__dirname, 'views/about.html'));
  })
  
+
+
+ ///blog
  app.get('/blog', (req, res) => {
-     service.getPublishedPosts().then(data => res.json(data)).catch(err => res.json({"message: err" : err}));
+     service.getPublishedPosts().then((data)=>{res.json({data});
+    }).catch ((err)=>{res.json({"message: err" : err})});
  })
- 
+
+
+ //posts
+ //then(data => res.json(data)).catch(err => res.json({"message: err" : err}));
  app.get('/posts', (req, res) => {
-     service.getAllPosts().then(data => res.json(data)).catch(err => res.json({"message: err" : err}));
- })
+    service.getAllposts().then((data)=>{res.json({data});
+}).catch ((err)=>{res.json({"message: err" : err})});
+})
  
- app.get('/categories', (req, res) => {
+
+//categories
+app.get('/categories', (req, res) => {
      service.getCategories().then(data => res.json(data)).catch(err => res.json({"message: err" : err}));
- })
+})
+
  
- app.use(function (req, res) {
+
+
+
+//[ no matching route ]
+ 
+app.use(function (req, res)
+{
   res.status(404).send('ERROR : Page Not Found');
 });
 
 
- app.listen(HTTP_PORT, () => {
-     service.initialize()
-     .then(function () {
-       app.listen(HTTP_PORT, onHTTPStart);
-     })
-     .catch(function (err) {
-       console.log('SORRY!! not able to read the file ' + err);
-     });
- })
+service.initialize().then(()=>
+{
+    //the promise will reject if any error occurred during the process
+
+        app.listen(HTTP_PORT, onHttpStart);
+}).catch((err) => 
+
+{
+    console.log('SORRY!! not able to read the file '+ err)
+
+})
