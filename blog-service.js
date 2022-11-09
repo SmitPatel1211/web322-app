aeb6c80f8c96e58ae12c24b06f201e09c8d3f52c
SmitPatel1211 <sonu9408394224@gmail.com>
2022-11-09 17:46:54 -0500

    new service added

--- blog-service.js
+++ blog-service.js
@@ -1,198 +1,119 @@
-//the fs.readFile method
-
-const fs = require('fs');
-
-//•	posts - type: array
+const fs = require("fs");
 
 let posts = [];
-
-//•	categories - type: array
 let categories = [];
-
-
-const multer = require('multer');
-const cloudinary = require('cloudinary').v2 ;
-const streamifier = require('streamifier') ;
-
-
 
 module.exports.initialize = function () {
     return new Promise((resolve, reject) => {
+        fs.readFile('./data/posts.json', 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                posts = JSON.parse(data);
 
-      /*fs.readFile('somefile.json', 'utf8', (err, data) => {
-     if (err) throw err;
-     console.log(data);
-    }); */
-  
-      fs.readFile('./data/posts.json', (err, data) => {
-        if (err) {
-          reject(err);
-        }
-        //assign that array to the posts array 
-
-        posts = JSON.parse(data);
-       
-      });
-      fs.readFile('./data/categories.json', (err, data) => {
-        if (err) {
-          reject(err);
-        }
-        categories = JSON.parse(data);
-        
-      });
-      resolve();
+                fs.readFile('./data/categories.json', 'utf8', (err, data) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        categories = JSON.parse(data);
+                        resolve();
+                    }
+                });
+            }
+        });
     });
-  };
-  
+}
 
-  //•	This function will provide the full array of "posts" objects using the resolve method of the 
-  //returned promise.  
-
-  module.exports.getAllPosts = function () {
-
-    return new Promise(
-        (resolve, reject) => {
-
-      if (posts.length == 0) {
-
-        //the reject method 
-        reject('NO RESULTS RETURNED'
-        );
-      }
-      resolve(posts);
-    });
-  };
-  
-  
-  //getPublishedPosts()
-  /**•	This function will provide an array of "post" objects whose published property is true using the resolve method of the returned promise.  / */
-
-  module.exports.getPublishedPosts = function ()
-   {
+module.exports.getAllPosts = function(){
     return new Promise((resolve,reject)=>{
-
-      const arr1=posts.filter(p=>p.published==true);
-
-      if(posts.length==0){
-
-       reject('no results returned');
-
-      }
-      else{
-
-       resolve(arr1);
-
-      }
-   })
-  };
-  
-//getCategories()
-//•	This function will provide the full array of "category" objects using the resolve method of the 
-//returned promise.  
-  
-  module.exports.getCategories = function () {
-    return new Promise((resolve, reject) => {
-      if (categories.length == 0) {
-        reject('NO RESULTS RETURNED');
-      }
-      resolve(categories);
+        (posts.length > 0 ) ? resolve(posts) : reject("no results returned"); 
     });
-  };
+}
 
-
-  module.exports.addPost=(postData)=>{
-    //o	/posts?category=value 
-    postData.published == undefined ? postData.published=false : postData.published=true;
-    postData.id=post.length+1;
-    post.push(postData);
-   
-    return new Promise((resolve,reject)=>{//return a Promise
-
-        if(post.length==0){
-
-            reject('NO RESULTS!!!');
-
-        }
-
-        else if(post.length<0){
-
-          reject('Invalid value');
-
-      }
-
-
-        else{
-            resolve(post);
-        }
+module.exports.getPostsByCategory=(postcatId)=>{
+    return new Promise((resolve,reject)=>{
+     var pcategoryid=[];
+     for(let s =0; s < posts.length;s++){
+         if(posts[s].category==postcatId){
+             pcategoryid.push(posts[s]);
+         }
+     }
+     if(pcategoryid){
+         resolve(pcategoryid);
+     }else{
+         reject("no results returned");
+     }
     })
-  }
-  // the getPostsByCategory(category) function
-  module.exports.getCategoryByPost = (id) => {
-    return new Promise((resolve, reject) => {
-      for (let s = 0; s < categories.length; s++) {
-        var catData;
-        if (categories[s].id == id) {
-          catData = categories[s];
-        }
-      }
-  
-      if (catData) {
-        resolve(catData);
-      } 
+ } 
+
+module.exports.getPostsByMinDate=(minDateStr)=>{
+    var pdateData=[];
+
+    return new Promise((resolve,reject)=>{
       
-      
-      
-      else {
-        reject("CATEGORY NOT FOUND!!!!!!");
-      }
-    })
-  }
- // Add the getPostsByMinDate(minDateStr) Function  
-  module.exports.getPostsByMinDate = function (minDateStr) {
-    var min_Date_Post = [];
-    var promise = new Promise((resolve, reject) =>
-     {
-      if (post.length === 0) {
-        reject("ERROR");
-      }
-      else {
-        for (var s = 0; s < post.length; s++) {
-
-
-          if (new Date(post[s].postDate) >= new Date(minDateStr))
-          {
-            min_Date_Post.push(post[s]);
-          }
-          else if (new Date(post[s].postDate) <= new Date(minDateStr))
-          {
-            return 0;
-          }
+        for(let s =0; s < posts.length; s++){
+        if(new Date(posts[s].postDate) >= new Date(minDateStr)){
+            console.log("The postDate is more than the minimumdateSTR")
+            pdateData.push(posts[s]);
         }
-      }
-      resolve(min_Date_Post);
+        resolve(pdateData);
+    }    
     })
-    return promise;
+}
+
+module.exports.getPostById=(id)=>{
+    return new Promise((resolve,reject)=>{
+      var post_id;
+      for(let i=0;i<posts.length;i++){
+          if(posts[i].id==id){
+              post_id=posts[i];
+          }
+      }
+      if(post_id){
+          resolve(post_id);
+      }else{
+          reject("id not found");
+      }
+    })
   }
 
-  module.exports.getPostsByCategory = function (category) {
-    var postCat = [];
-    var promise = new Promise((resolve, reject) => {
-      if (post.length === 0) {
-        reject("no results returned");
-      }
-      else {
-        for (var temp = 0; temp < post.length; temp++) {
-          if (post[temp].category == category) {
-            postCat.push(post[temp])
-          }
+module.exports.addPost = function(postData){
+    return new Promise((resolve,reject)=>{
+        postData.published = postData.published ? true : false;
+        postData.id = posts.length + 1;
+        posts.push(postData);
+        resolve();
+    });
+}
 
-          else if (post[temp].category != category) {
-            resolve(postCat);
-          }
+module.exports.getPublishedPosts = function(){
+    return new Promise((resolve,reject)=>{
+        let filteredPosts = posts.filter(post => post.published);
+        (filteredPosts.length > 0) ? resolve(filteredPosts) : reject("no results returned");
+    });
+}
+
+module.exports.getCategories = function(){
+    return new Promise((resolve,reject)=>{
+        (categories.length > 0 ) ? resolve(categories) : reject("no results returned"); 
+    });
+}
+
+
+module.exports.getPublishedPostsByCategory=(category)=>{
+    return new Promise((resolve,reject)=>{
+        const arrCheck=posts.filter(tc=>tc.published==true && tc.category==category);
+        if(posts.lenght==0){
+
+            reject("NO result RETURNED");
+
         }
-        resolve(postCat);
-      }
+        
+
+      resolve(arrCheck);
+
+        
     })
-    return promise;
-  }
+
+}
 
